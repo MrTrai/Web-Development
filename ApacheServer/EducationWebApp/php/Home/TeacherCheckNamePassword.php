6a302731ef9ea3fd1f3235500cfe30d709df3bac
Ttran96 <ttran96@gatech.edu>
2016-12-18 19:55:36 -0500

    php

---
+++ ApacheServer/EducationWebApp/php/Home/TeacherCheckNamePassword.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$logName = $_REQUEST['username'];
+$logPassword = $_REQUEST['password'];
+
+//Insert
+$sql = "SELECT Username, Password, Email
+    FROM Teachers
+    WHERE Username='$logName'
+    AND Password='$logPassword'";
+
+//check error
+$result = mysqli_query($conn, $sql);
+$emparray = array();
+if (mysqli_num_rows($result) > 0) {
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+
+    echo json_encode($emparray);
+} else {
+    echo "False";
+}
+
+
+mysqli_close($conn);
+?>
